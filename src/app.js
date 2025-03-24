29b0df245f6cdcd85b1c18637864b8f59ec5d9b6
Eyub-Abdi <ayubabdiy@gmail.com>
2025-03-24 22:12:24 +0300

    Add more routes

--- src/app.js
+++ src/app.js
@@ -4,9 +4,11 @@ const config = require('config')
 const db = require('./db/knex')
 const app = express()
 const customerRoutes = require('./routes/customerRoutes')
+const authRoutes = require('./routes/authRoutes')
 
 app.use(express.json())
 app.use('/api/customers', customerRoutes)
+app.use('/api/auth', authRoutes)
 
 app.get('/', (req, res) => res.send('Home'))
 debug(config.get('db.password'))
