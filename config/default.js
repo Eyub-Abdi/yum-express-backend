6e543787b109c0477d6b72085dc77aa955681774
Eyub-Abdi <ayubabdiy@gmail.com>
2025-03-24 22:09:38 +0300

    Add jwt configurations

--- config/default.js
+++ config/default.js
@@ -4,5 +4,9 @@ module.exports = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET || 'your-secret-key', // Default secret if no env var is set
+    expiresIn: process.env.JWT_EXPIRES_IN || '1h' // Default expiration time if no env var is set
   }
 }
