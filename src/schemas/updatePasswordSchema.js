dd5fd490b0dfc6711d3321159b43f3be79abde22
Eyub-Abdi <ayubabdiy@gmail.com>
2025-03-26 20:46:16 +0300

    Build password update schema

---
+++ src/schemas/updatePasswordSchema.js
@@ -0,0 +1,9 @@
+// src/schemas/updatePasswordSchema.js
+const Joi = require('joi')
+
+const updatePasswordSchema = Joi.object({
+  old_password: Joi.string().required(),
+  new_password: Joi.string().min(6).required()
+})
+
+module.exports = { updatePasswordSchema }
