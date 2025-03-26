94297f2094e4670474e31c9ef1cfdecf03f22224
Eyub-Abdi <ayubabdiy@gmail.com>
2025-03-26 20:51:17 +0300

    Add crud routes for vendors

--- src/routes/vendorRoutes.js
+++ src/routes/vendorRoutes.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const router = express.Router()
-const { registerVendor } = require('../controllers/vendorController') // Import the vendor controller
+const { registerVendor, getVendors, getVendorById, updateVendor, updateVendorEmail, deleteVendor, updateVendorPassword, deactivateOwnVendorAccount } = require('../controllers/vendorController') // Import the vendor controller
 const { verifyVendorEmail } = require('../controllers/vendorController')
 
-// Vendor registration route
+// Vendor routes
 router.post('/register', registerVendor)
 router.get('/verify-email', verifyVendorEmail)
+router.get('/', getVendors)
+router.get('/:id', getVendorById)
+router.put('/:id', updateVendor)
+router.put('/update-email/:id', updateVendorEmail)
+router.put('/update-password', updateVendorPassword)
+router.put('/deactivate-account', deactivateOwnVendorAccount)
+router.delete('/:id', deleteVendor)
 
 module.exports = router
