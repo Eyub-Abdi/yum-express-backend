42291022f772bc45d78ab1bc6157ff42277aabb4
Eyub-Abdi <ayubabdiy@gmail.com>
2025-03-26 13:24:29 +0300

    Rebuild token generation logic with improvements and flexibility

--- src/services/tokenService.js
+++ src/services/tokenService.js
@@ -1,13 +1,13 @@
-const crypto = require('crypto') // Importing crypto for random token generation
+const crypto = require('crypto')
 
 // Function to generate a random verification token
 const generateVerificationToken = () => {
   return crypto.randomBytes(20).toString('hex') // Generate a random 20-byte string
 }
 
-// Function to generate token expiry time (1 hour validity)
-const generateVerificationTokenExpiry = () => {
-  return new Date(Date.now() + 3600000) // Token valid for 1 hour
+// Function to generate token expiry time (takes validity in hours as a parameter)
+const generateVerificationTokenExpiry = (validityInHours = 1) => {
+  return new Date(Date.now() + validityInHours * 3600000) // Token valid for the given number of hours
 }
 
 module.exports = {
