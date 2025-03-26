17b2efd11be2c37d40a3ca077ee10156cea70531
Eyub-Abdi <ayubabdiy@gmail.com>
2025-03-26 13:28:45 +0300

    Add vendor email verification route

--- src/routes/vendorRoutes.js
+++ src/routes/vendorRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const { registerVendor } = require('../controllers/vendorController') // Import the vendor controller
+const { verifyVendorEmail } = require('../controllers/vendorController')
 
 // Vendor registration route
 router.post('/register', registerVendor)
+router.get('/verify-email', verifyVendorEmail)
 
 module.exports = router
