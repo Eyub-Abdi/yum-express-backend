1f4a81c2bf6592ce7aa53f9b0ea19414f32c7ff3
Eyub-Abdi <ayubabdiy@gmail.com>
2025-03-28 11:04:57 +0300

    Refactor code

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,9 +1,8 @@
 // src/routes/productRoutes.js
 const express = require('express')
+const router = express.Router()
 const { addProduct, getProducts, getProductById, getMyProducts, updateProduct, deleteProduct } = require('../controllers/productController')
 const authenticateUser = require('../middleware/authenticateUser')
-
-const router = express.Router()
 
 router.post('/', authenticateUser, addProduct) // Only authenticated vendors can add products
 router.get('/my-store', authenticateUser, getMyProducts)
