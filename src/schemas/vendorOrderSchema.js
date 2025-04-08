d242ecd6ee64e3a5d7b50c291b5410eb4714bd94
Eyub-Abdi <ayubabdiy@gmail.com>
2025-04-08 23:32:03 +0300

    Build vendor order schemas

---
+++ src/schemas/vendorOrderSchema.js
@@ -0,0 +1,20 @@
+const Joi = require('joi')
+
+const getVendorOrdersSchema = Joi.object({
+  status: Joi.string().valid('pending', 'shipped', 'delivered', 'canceled').optional(),
+  start_date: Joi.date().iso().optional(), // ISO 8601 date format (e.g., 2025-04-01)
+  end_date: Joi.date().iso().optional(), // ISO 8601 date format (e.g., 2025-04-30)
+  product_name: Joi.string().optional(), // Product name can be any string
+  customer_name: Joi.string().optional() // Customer name can be any string
+}).with('start_date', 'end_date') // If start_date is provided, end_date should be present too
+
+const updateVendorOrderStatusSchema = Joi.object({
+  status: Joi.string().valid('pending', 'shipped', 'delivered').required(),
+
+  tracking_number: Joi.string().optional()
+})
+
+module.exports = {
+  getVendorOrdersSchema,
+  updateVendorOrderStatusSchema
+}
