2eb1b7762900c434d79aeb0c75050cbea78143b6
Eyub-Abdi <ayubabdiy@gmail.com>
2025-04-27 00:17:59 +0300

    Adjusted the schema to accept country code without + sign

--- src/schemas/customerSchema.js
+++ src/schemas/customerSchema.js
@@ -6,8 +6,8 @@ const customerRegistrationSchema = Joi.object({
   last_name: Joi.string().min(3).max(50).required(),
   email: Joi.string().email().required(),
   phone: Joi.string()
-    .pattern(/^[0-9]{10}$/)
-    .required(), // Example for a 10-digit phone number
+    .pattern(/^255\d{9}$/) // starts with 255, followed by exactly 9 digits
+    .required(),
   password: Joi.string().min(6).required()
 })
 
