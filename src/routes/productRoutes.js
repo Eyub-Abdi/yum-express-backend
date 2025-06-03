a4074a4224bc629996ece054c47f0d92b4eeedeb
Eyub-Abdi <ayubabdiy@gmail.com>
2025-06-03 23:44:51 +0300

    Install file middleware

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,10 +1,11 @@
 // src/routes/productRoutes.js
 const express = require('express')
 const router = express.Router()
+const upload = require('../middleware/upload')
 const { addProduct, getProducts, getProductById, getMyProducts, updateProduct, deleteProduct } = require('../controllers/productController')
 const authenticateUser = require('../middleware/authenticateUser')
 
-router.post('/', authenticateUser, addProduct) // Only authenticated vendors can add products
+router.post('/', authenticateUser, upload.single('image'), addProduct) // Only authenticated vendors can add products
 router.get('/my-store', authenticateUser, getMyProducts)
 router.put('/:id', authenticateUser, updateProduct)
 router.get('/', getProducts)
