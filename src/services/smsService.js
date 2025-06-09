feb721cdd624af2d10e70ddb5c10e7c98a8c6ee8
Eyub-Abdi <ayubabdiy@gmail.com>
2025-06-09 13:36:54 +0300

    Integrate Nest SMS

--- src/services/smsService.js
+++ src/services/smsService.js
@@ -1,16 +1,52 @@
-const twilio = require('twilio')
+const axios = require('axios')
 const config = require('../../config/default')
 
-const client = twilio(config.sms.accountSid, config.sms.authToken)
+function getAuthHeader(username, password) {
+  console.log(username + ':' + password)
+  const base64 = Buffer.from(`${username}:${password}`).toString('base64')
+  return `Basic ${base64}`
+}
 
-function sendOrderConfirmationSMS(toPhoneNumber, message) {
-  return client.messages.create({
-    body: message,
-    from: config.sms.phoneNumber,
-    to: toPhoneNumber
+function getSMSApiUrl() {
+  return config.sms.isTestMode ? 'https://messaging-service.co.tz/api/sms/v1/test/text/single' : 'https://messaging-service.co.tz/api/sms/v1/text/single'
+}
+
+/**
+ * Sends an SMS message using Next SMS API
+ * @param {string} toPhoneNumber
+ * @param {string} message
+ * @param {string} [reference='yum-express-ref']
+ * @returns {Promise<Object>}
+ */
+function sendSMS(toPhoneNumber, message, reference = 'yum-express-ref') {
+  const data = {
+    from: '255684106419', //config.sms.senderId,
+    to: toPhoneNumber,
+    text: message,
+    reference
+  }
+  // return console.log(console.log(data))
+  return axios({
+    method: 'post',
+    url: getSMSApiUrl(),
+    maxBodyLength: Infinity,
+    headers: {
+      Authorization: getAuthHeader(config.sms.username, config.sms.password),
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    data
   })
+    .then(res => {
+      console.log('SMS sent:', res.data)
+      return res.data
+    })
+    .catch(err => {
+      console.error('SMS failed:', err.response?.data || err.message)
+      throw err
+    })
 }
 
 module.exports = {
-  sendOrderConfirmationSMS
+  sendSMS
 }
